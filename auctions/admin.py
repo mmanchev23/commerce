3ef3fafcc29210198a10dbd7280841cde8f5f762
mmanchev23 <mmanchev2003@gmail.com>
2021-07-18 00:43:47 +0300

    Last modification on the models and admin panel.

--- auctions/admin.py
+++ auctions/admin.py
@@ -3,3 +3,9 @@ from .models import *
 
 
 admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
+admin.site.register(ClosedBid)
+admin.site.register(AllListing)
